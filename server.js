c531b918295d6e9b6c7ac1f7410959627656cf39
Casey MacLeod <caseymacleod22@gmail.com>
2022-07-16 22:54:34 -0400

    connected to database

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('test')
+// Import MongoDB
+const mongoose = require('mongoose' )
+const dbURI = 'mongodb+srv://admin:pepper22@cluster0.esjr1.mongodb.net/fantasy-football-newsletter'
+
+// Connect to DB
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}) .then(function(result) {
+    console.log('database is connected')
+}) .catch((err) => console.log(err))
