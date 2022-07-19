096518cb947906589622ae852691d5a20115655e
Casey MacLeod <caseymacleod22@gmail.com>
2022-07-19 17:10:08 -0400

    adds create and delete routes

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -10,4 +10,42 @@ const commentGetAll = (req, res) => {
     })
 }
 
-module.exports = {commentGetAll}
+const comment_get_byID = (req, res) => {
+    const id = req.params.id;
+    Comment.findById(id)
+        .then(result => {
+            res.status(200).send(result);
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        });
+}
+
+const comment_create = (req, res) => {
+    const comment = new Comment(req.body);
+    comment.save()
+        .then(result => {
+            res.status(201).send(result);
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        });
+}
+
+const comment_delete = (req, res) => {
+    const id = req.params.id;
+    Comment.findByIdAndDelete(id)
+        .then(result => {
+            res.status(200).send(result);
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        });
+}
+
+module.exports = {
+    commentGetAll,
+    comment_get_byID,
+    comment_create,
+    comment_delete,
+}
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -4,5 +4,8 @@ const commentController = require('../controllers/commentController')
 const router = express.Router()
 
 router.get('/', commentController.commentGetAll)
+router.get('/:id', commentController.comment_get_byID)
+router.post('/', commentController.comment_create)
+router.delete('/:id', commentController.comment_delete)
 
 module.exports = router
